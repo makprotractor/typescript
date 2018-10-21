82bf2019f111242d20a474da914bed805c7e59eb
Arun Mannepula <makprotractor@gmail.com>
2018-10-21 14:12:25 -0500

    Arun sample files on typescript learning ...

---
+++ app.ts
@@ -0,0 +1,18 @@
+let stud=  {
+    Name:"Partha", 
+    Age:301, 
+    Phone:911
+}
+
+let studentsList = [
+    {Name:"Prasanth", Age:26, Phone:12344},
+    {Name:"Krishna",Age:30,Phone:32434},
+    {Name:"Geetha",Age:35,Phone:2346787}
+]
+
+studentsList.push(stud);
+for(var i = 0; i < studentsList.length; i++ ){
+    var element = studentsList[i];
+ 
+    console.log(element);
+}
